c6e9cba285f183173c574179f61e433caf4a898f
victoriapong <victoria.pong@gmail.com>
2018-10-02 01:44:41 +0800

    :white_check_mark: Develop: ProgressBar TDD for stateful component and calculatePercentage method

--- src/__tests__/components/ProgressBar.test.js
+++ src/__tests__/components/ProgressBar.test.js
@@ -12,7 +12,44 @@ describe("Progress Bar Component", () => {
 
   it("should show progress value that has been passed through progressValue property", () => {
     const component = shallow(<ProgressBar progressValue={20} />);
-    expect(component.find("span")).toHaveLength(1);
-    expect(Number(component.find("span").text())).toEqual(20);
+    expect(component.find("span[data-progress='progress-value']")).toHaveLength(
+      1
+    );
+    expect(
+      Number(component.find("span[data-progress='progress-value']").text())
+    ).toEqual(20);
+  });
+
+  it("should show progress value that has been passed through progressValue property", () => {
+    const component = shallow(<ProgressBar progressValue={20} />);
+    expect(component.find("span[data-progress='progress-value']")).toHaveLength(
+      1
+    );
+    expect(
+      Number(component.find("span[data-progress='progress-value']").text())
+    ).not.toEqual(10);
+  });
+
+  it("should show limit value that has been passed through limit property", () => {
+    const component = shallow(<ProgressBar limit={200} />);
+    expect(component.find("span[data-progress='limit-value']")).toHaveLength(1);
+    expect(
+      Number(component.find("span[data-progress='limit-value']").text())
+    ).toEqual(200);
+  });
+
+  it("should have calculatePercentage function", () => {
+    const component = shallow(<ProgressBar />);
+    expect(component.instance().calculatePercentage()).toBeDefined();
+  });
+
+  it("calculatePercentage of progress 10 and limit 200 should equal to 5", () => {
+    const component = shallow(<ProgressBar />);
+    expect(component.instance().calculatePercentage(10, 200)).toEqual(5);
+  });
+
+  it("calculatePercentage of progress 62 and limit 230 should equal to 27", () => {
+    const component = shallow(<ProgressBar />);
+    expect(component.instance().calculatePercentage(62, 230)).toEqual(27);
   });
 });

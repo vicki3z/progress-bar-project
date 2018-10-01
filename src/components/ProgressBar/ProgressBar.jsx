f02d31f884997f04c1f67cdcb2446fd0897692c6
victoriapong <victoria.pong@gmail.com>
2018-10-02 01:45:21 +0800

    Update: ProgressBar - update to stateful component and add calculatePercentage method

--- src/components/ProgressBar/ProgressBar.jsx
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { Component } from "react";
 
-const ProgressBar = ({ progressValue }) => (
-  <div>
-    <span>{progressValue}</span>
-  </div>
-);
+class ProgressBar extends Component {
+  calculatePercentage(progress, limit) {
+    let percentage = (progress / limit) * 100;
+    return Math.round(percentage);
+  }
+
+  render() {
+    const { progressValue, limit } = this.props;
+    return (
+      <div>
+        <span data-progress="progress-value">{progressValue}</span>
+        <span data-progress="limit-value">{limit}</span>
+      </div>
+    );
+  }
+}
 
 export default ProgressBar;
